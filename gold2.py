0c585ac3908e9f63827d9a2d47f387910abef2cb
prollycause <prollycause@users.noreply.github.com>
2017-07-04 12:27:19 -0400

    removed more redundancy

--- gold2.py
+++ gold2.py
@@ -1,6 +1,5 @@
 from time import sleep
 from selenium import webdriver
-import os
 from selenium.webdriver.common.keys import Keys
 
 class GetGold():
@@ -11,7 +10,6 @@ class GetGold():
 		driver.get("https://goldfinder.site")
 		sleep(5)
 		driver.find_element_by_class_name("g-recaptcha").send_keys(Keys.RETURN)
-		
 		input("press a button when finished")		
 
 if __name__ == "__main__":
